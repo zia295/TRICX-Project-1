af86322aa693d12f901c99303915f9a073a69d40
zia295 <74332450+zia295@users.noreply.github.com>
2024-12-06 01:35:36 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include "header.php";
+?>
+
+
+<section class="main-body">
+    <div class="box-container">
+    <div class="box">
+        <div class="box-head">
+
+        main content here
+
+
+
+        </div>
+        </div>
+        </div>
+
+
+    </section>
+    
+</body>
+</html>
